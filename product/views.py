171c9111f4dc3dc7fe8916a2cd50cf9c4c84c247
Antu Saha <antusaha970@gmail.com>
2024-07-03 02:07:06 +0600

    added product images apis

--- product/views.py
+++ product/views.py
@@ -4,6 +4,10 @@ from . import serializers
 from . import models
 from .filters import ProductFilter
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 # Create your views here.
 
 
@@ -22,3 +26,23 @@ class ProductViewSet(viewsets.ModelViewSet):
         filterset = ProductFilter(
             self.request.GET, queryset=models.Product.objects.all().order_by('id'))
         return filterset.qs
+
+    @action(detail=True, methods=['POST'])
+    def upload_product_image(self, request, pk=None):
+        product = get_object_or_404(models.Product, pk=pk)
+
+        images = request.FILES.getlist('images')
+        serialized_data = []
+
+        for image in images:
+            data = {'product': product.id, 'images': image}
+            serialized_data.append(data)
+
+        serializer = serializers.ProductImageSerializer(
+            data=serialized_data, many=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

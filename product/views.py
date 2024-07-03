728b45c69828cb866e9cdee9d8461158cede7da6
Antu Saha <antusaha970@gmail.com>
2024-07-03 12:20:47 +0600

    added action method for categories,colors,sizes

--- product/views.py
+++ product/views.py
@@ -46,3 +46,21 @@ class ProductViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["GET"])
+    def all_categories(self, request):
+        categories = models.Category.objects.all()
+        serializer = serializers.CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=["GET"])
+    def all_sizes(self, request):
+        categories = models.Size.objects.all()
+        serializer = serializers.SizeSerializer(categories, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=["GET"])
+    def all_colors(self, request):
+        categories = models.Color.objects.all()
+        serializer = serializers.ColorSerializer(categories, many=True)
+        return Response(serializer.data)

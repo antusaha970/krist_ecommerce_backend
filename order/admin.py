6ec01b19ed5c6ce90b53752aa5706292a32b4aee
Antu Saha <antusaha970@gmail.com>
2024-09-10 22:12:22 +0600

    added order models in admin panel

--- order/admin.py
+++ order/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import DeliveryAddress
+from .models import DeliveryAddress, Order, OrderItem
 
 admin.site.register(DeliveryAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
